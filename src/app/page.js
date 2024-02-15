99a3b5809268f10e19abfc3020f18db3e7fd1c06
GlaucoStarling <glaucox.x@gmail.com>
2024-02-15 08:35:24 -0300

    foi

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import NavBar from "@/components/NavBar"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <NavBar/>
+      <h1>Uma historia de amor</h1>
+      <div className="text-center">
+        <img src="https://lh3.googleusercontent.com/pw/ABLVV85kcc2NDMUlPtwfE-5wkJSMocEJqdjLPS334Ct5n3GgSQy1J0z4Wnf00agbKLYzcwLWJvTeBYX53xSN8ySbo2olqv-KEmBsffEUFIXNukHfUqEqehzQDfLwJXuX4TL1uUBjhYftvPIFgUI2GHkeVwkV=w718-h957-s-no-gm?authuser=0"
+          className="img-thumbnail rounded " 
+          alt="..."
+        ></img>
+      </div>
+      <div>
+        <p> Uma introducao calorosa </p>
+      </div>
+      <div className="poem">
+        <h1>O Tempo Do Amor</h1>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Quando o amor verdadeiro é encontrado,<br />
+          O tempo se torna um mero espectador,<br />
+          Sem fronteiras ou limites a separar,<br />
+          Nossos corações unidos a florescer.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <p>
+          As estações podem mudar, os anos passar,<br />
+          Mas o amor permanece, firme e constante,<br />
+          Como um rio fluindo eternamente,<br />
+          Nenhum obstáculo pode quebrar seu encanto.
+        </p>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <p>
+          Com a bênção de Deus, nosso amor é abençoado,<br />
+          Uma aliança divina, em Seu amor imerso,<br />
+          Guiados por Sua luz, seguimos adiante,<br />
+          Em um caminho de graça, onde o amor é universo.
+        </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <p>
+          No relógio do universo, não há marcação,<br />
+          Apenas o pulsar dos corações entrelaçados,<br />
+          Cada batida uma promessa de eternidade,<br />
+          Um laço que nunca pode ser desatado.
+        </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+        <p>Então, abraçados sob o manto do céu,<br />
+        Desafiando as correntes do destino,<br />
+        Nosso amor transcende o efêmero,<br />
+        Para além do tempo, eternamente divino.</p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p>Que este amor seja nossa estrela-guia,<br />
+        Navegando pelos mares da eternidade,<br />
+        Em um reino onde o tempo não é senhor,<br />
+        Onde nosso amor brilha em sua plenitude.</p>
+
+        <p>Com a bênção de Deus sobre nós, avançamos,<br />
+        Em comunhão sagrada, nossa fé fortalecida,<br />
+        Para vivermos este amor, em toda sua plenitude,<br />
+        Em um eterno abraço, na luz de Sua guarida.</p>
       </div>
     </main>
   );
